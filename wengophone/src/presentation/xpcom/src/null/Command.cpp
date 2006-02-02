cf0244320455cc7a58eada835b4a851f16a2d4f3
Tanguy Krotoff <tkrotoff@gmail.com>
2006-02-02 19:34:21 +0000

    Stub implementation for xpcomwengophone

--- wengophone/src/presentation/xpcom/src/null/Command.cpp
+++ wengophone/src/presentation/xpcom/src/null/Command.cpp
@@ -20,25 +20,124 @@
 #include <Command.h>
 #include <Listener.h>
 
-Command::Command(const std::string & configFilesPath) { }
-Command::~Command() { }
-void Command::start() { }
+#include <string>
+#include <iostream>
+
+void LOG_DEBUG(const std::string & message = "") {
+	std::string tmp = "(debug)\t" + std::string(__FUNCTION__) + ": " + message;
+	std::cerr << tmp << std::endl;
+}
+
+
+static Listener * _listener = NULL;
+static std::string _login;
+static std::string _password;
+
+Command::Command(const std::string & configFilesPath) {
+	LOG_DEBUG();
+}
+
+Command::~Command() {
+	LOG_DEBUG();
+}
+
+void Command::start() {
+	LOG_DEBUG();
+	if (_listener) {
+		_listener->wengoLoginStateChangedEvent(Listener::LoginOk, _login, _password);
+		_listener->phoneLineStateChangedEvent(Listener::LineOk, 1, NULL);
+	}
+}
+
 void Command::setHttpProxySettings(const std::string & hostname, unsigned port,
-			const std::string & login, const std::string & password) { }
-void Command::terminate() { }
-void Command::addWengoAccount(const std::string & login, const std::string & password, bool autoLogin) { }
-void Command::addListener(Listener * listener) { }
-bool Command::removeListener(Listener * listener) { }
-void Command::removeAllListeners() { }
-int Command::makeCall(const std::string & phoneNumber, int lineId) { }
-void Command::hangUp(int callId) { }
-bool Command::canHangUp(int callId) const { }
-void Command::pickUp(int callId) { }
-bool Command::canPickUp(int callId) const { }
-void Command::subscribeToPresenceOf(const std::string & contactId) { }
-void Command::publishMyPresence(Listener::PresenceState state, const std::string & note) { }
-int Command::sendChatMessage(const std::string & contactId, const std::string & message) { }
-void Command::setInputVolume(int volume) { }
-int Command::getInputVolume() { }
-int Command::getOutputVolume() { }
-void Command::setOutputVolume(int volume) { }
+			const std::string & login, const std::string & password) {
+
+	LOG_DEBUG();
+	_login = login;
+	_password = password;
+}
+
+void Command::terminate() {
+	LOG_DEBUG();
+}
+
+void Command::addWengoAccount(const std::string & login, const std::string & password, bool autoLogin) {
+	LOG_DEBUG();
+}
+
+void Command::addListener(Listener * listener) {
+	LOG_DEBUG();
+	_listener = listener;
+}
+
+bool Command::removeListener(Listener * listener) {
+	LOG_DEBUG();
+	return true;
+}
+
+void Command::removeAllListeners() {
+	LOG_DEBUG();
+}
+
+int Command::makeCall(const std::string & phoneNumber, int lineId) {
+	LOG_DEBUG();
+	if (_listener) {
+		_listener->phoneCallStateChangedEvent(Listener::CallOk, 1, 1,
+					"sip:toto@toto.com", "toto", "toto", NULL);
+	}
+
+	return 1;
+}
+
+void Command::hangUp(int callId) {
+	LOG_DEBUG();
+	if (_listener) {
+		_listener->phoneCallStateChangedEvent(Listener::CallClosed, 1, 1,
+					"sip:toto@toto.com", "toto", "toto", NULL);
+	}
+}
+
+bool Command::canHangUp(int callId) const {
+	LOG_DEBUG();
+	return true;
+}
+
+void Command::pickUp(int callId) {
+	LOG_DEBUG();
+}
+
+bool Command::canPickUp(int callId) const {
+	LOG_DEBUG();
+	return true;
+}
+
+void Command::subscribeToPresenceOf(const std::string & contactId) {
+	LOG_DEBUG();
+}
+
+void Command::publishMyPresence(Listener::PresenceState state, const std::string & note) {
+	LOG_DEBUG();
+}
+
+int Command::sendChatMessage(const std::string & contactId, const std::string & message) {
+	LOG_DEBUG();
+	return 1;
+}
+
+void Command::setInputVolume(int volume) {
+	LOG_DEBUG();
+}
+
+int Command::getInputVolume() {
+	LOG_DEBUG();
+	return 0;
+}
+
+int Command::getOutputVolume() {
+	LOG_DEBUG();
+	return 0;
+}
+
+void Command::setOutputVolume(int volume) {
+	LOG_DEBUG();
+}
