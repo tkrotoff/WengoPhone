80c02a8f2db16ad34eb80739a27b9d4721142d14
Mathieu Stute <mathieu.stute@wengo.com>
2006-04-24 15:16:01 +0000

    remove "Subscribe to the PSTN unlimited" in the profile bar

--- wengophone/src/presentation/qt/profilebar/QtCreditWidget.cpp
+++ wengophone/src/presentation/qt/profilebar/QtCreditWidget.cpp
@@ -13,13 +13,13 @@ QtCreditWidget::QtCreditWidget(QWidget * parent , Qt::WFlags f ) : QWidget(paren
 	QGridLayout * gridLayout = new QGridLayout(this);
 
 	QtClickableLabel * buy = new QtClickableLabel(this);
-	QtClickableLabel * subscribe = new QtClickableLabel(this);
+	//QtClickableLabel * subscribe = new QtClickableLabel(this);
 
 	buy->setText( tr ("Buy call-out credits") );
-	subscribe->setText( tr ("Subscribe to the PSTN unlimited") );
+	//subscribe->setText( tr ("Subscribe to the PSTN unlimited") );
 
 	gridLayout->addWidget(buy,0,0);
-	gridLayout->addWidget(subscribe,1,0);
+	//gridLayout->addWidget(subscribe,1,0);
 
 	connect ( buy, SIGNAL ( clicked() ), SLOT ( buyOutClicked() ) );
 
