21a2f2ecdaf0063e0c62c65813d5678df379b788
Mathieu Stute <mathieu.stute@wengo.com>
2006-05-05 13:26:34 +0000

    work on call forward

--- wengophone/src/presentation/qt/config/QtCallForwardSettings.cpp
+++ wengophone/src/presentation/qt/config/QtCallForwardSettings.cpp
@@ -24,6 +24,9 @@
 #include <model/config/ConfigManager.h>
 #include <model/config/Config.h>
 #include <control/CWengoPhone.h>
+#include <control/webservices/callforward/CWsCallForward.h>
+
+#include <util/Logger.h>
 
 #include <QtGui>
 
@@ -48,11 +51,55 @@ QString QtCallForwardSettings::getName() const {
 
 void QtCallForwardSettings::saveConfig() {
 
+	bool mustCallTheWs = false;
+	std::string mode = "";
+	std::string number1 =  _ui->phoneNumber1Edit->text().toStdString();
+	std::string number2 =  _ui->phoneNumber2Edit->text().toStdString();
+	std::string number3 =  _ui->phoneNumber3Edit->text().toStdString();
+	
+	if(_ui->forwardToVoiceMailRadioButton->isChecked()) {
+		mode = "voicemail";
+	} else if( _ui->disableRadioButton->isChecked() ) {
+		mode = "disable";
+	} else if( _ui->forwardToNumberRadioButton->isChecked() ) {
+		mode = "number";
+	}
+	
 	Config & config = ConfigManager::getInstance().getCurrentConfig();
+	
+	//if the mode has changed
+	if( config.getCallForwardMode() != mode ) {
+		config.set(Config::CALL_FORWARD_MODE_KEY, mode);
+		mustCallTheWs = true;
+	} else {
+		
+		//if numbers have changed
+		if( (mode == "number") &&
+			(config.getCallForwardPhoneNumber1() != number1 ) ||
+			(config.getCallForwardPhoneNumber2() != number2 ) ||
+			(config.getCallForwardPhoneNumber3() != number3))
+		{
+			config.set(Config::CALL_FORWARD_PHONENUMBER1_KEY, number1);
+			config.set(Config::CALL_FORWARD_PHONENUMBER2_KEY, number2);
+			config.set(Config::CALL_FORWARD_PHONENUMBER3_KEY, number3);
+			mustCallTheWs = true;
+		}
+	}
 
-	config.set(Config::CALL_FORWARD_PHONENUMBER1_KEY, _ui->phoneNumber1Edit->text().toStdString());
-	config.set(Config::CALL_FORWARD_PHONENUMBER2_KEY, _ui->phoneNumber2Edit->text().toStdString());
-	config.set(Config::CALL_FORWARD_PHONENUMBER3_KEY, _ui->phoneNumber3Edit->text().toStdString());
+	if( mustCallTheWs ) {
+		if( _cWengoPhone.getCWsCallForward() ) {
+			
+			if( mode == "voicemail" ) {
+				_cWengoPhone.getCWsCallForward()->forwardToVoiceMail();
+			} else if( mode == "disable" ) {
+				_cWengoPhone.getCWsCallForward()->disableCallForward();
+			} else if( mode == "number" ) {
+				_cWengoPhone.getCWsCallForward()->forwardToNumber(number1, number2, number3);
+			}
+		}
+	}
+
+	//unused for now
 	config.set(Config::CALL_FORWARD_TOMOBILE_KEY, _ui->forwardCallMobilCheckBox->isChecked());
 }
 
@@ -62,16 +109,19 @@ void QtCallForwardSettings::readConfig() {
 
 	if( config.getCallForwardMode() == "voicemail") {
 		_ui->forwardToVoiceMailRadioButton->setChecked(true);
+
 	}  else if( config.getCallForwardMode() == "number" ) {
 		_ui->forwardToNumberRadioButton->setChecked(true);
+
 	} else if( config.getCallForwardMode() == "disable" ) {
 		_ui->disableRadioButton->setChecked(true);
+
 	} else if( config.getCallForwardMode() == "unauthorized") {
 		_ui->groupBoxSettings->setEnabled(false);
 		_ui->groupBoxCellPhone->setEnabled(false);
 	}
+
 	_ui->forwardToVoiceMailRadioButton->setEnabled(config.hasVoiceMail());
-	
 	_ui->phoneNumber1Edit->setText(QString::fromStdString(config.getCallForwardPhoneNumber1()));
 	_ui->phoneNumber2Edit->setText(QString::fromStdString(config.getCallForwardPhoneNumber2()));
 	_ui->phoneNumber3Edit->setText(QString::fromStdString(config.getCallForwardPhoneNumber3()));
