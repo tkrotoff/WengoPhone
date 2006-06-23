f1e058668df59b080028f753b9f6693904fc0818
Mathieu Stute <mathieu.stute@wengo.com>
2006-06-23 12:49:29 +0000

    compilation fix

--- wengophone/src/presentation/qt/imcontact/QtSimpleAddIMContact.cpp
+++ wengophone/src/presentation/qt/imcontact/QtSimpleAddIMContact.cpp
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "QtSimpleAddImContact.h"
+#include "QtSimpleAddIMContact.h"
 
 #include <presentation/qt/profile/QtProfileDetails.h>
 
