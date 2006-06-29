bb7b6532b26b10110481fd245adc1edaa2021086
Tanguy Krotoff <tkrotoff@gmail.com>
2006-06-29 09:54:33 +0000

    DirectXWebcamUtils.cpp re-indented

--- libs/webcam/src/directx/DirectXWebcamUtils.cpp
+++ libs/webcam/src/directx/DirectXWebcamUtils.cpp
@@ -22,285 +22,281 @@
 #include <iostream>
 using namespace std;
 
-HRESULT FindMyCaptureDevice(IBaseFilter **pF, BSTR bstrName)
-{
-    HRESULT hr = E_FAIL;
-    HRESULT hr_work = E_FAIL;
-    CComPtr<IBaseFilter> pFilter;
-    CComPtr<ICreateDevEnum> pSysDevEnum;
-    CComPtr<IEnumMoniker> pEnumCat = NULL;
+HRESULT FindMyCaptureDevice(IBaseFilter * * pF, BSTR bstrName) {
+	HRESULT hr = E_FAIL;
+	HRESULT hr_work = E_FAIL;
+	CComPtr < IBaseFilter > pFilter;
+	CComPtr < ICreateDevEnum > pSysDevEnum;
+	CComPtr < IEnumMoniker > pEnumCat = NULL;
 
-    // Create the System Device Enumerator.
-    pSysDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
-    if (!pSysDevEnum) {
-        return E_FAIL;
-    }
+	// Create the System Device Enumerator.
+	pSysDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
+	if (!pSysDevEnum) {
+		return E_FAIL;
+	}
 
-    // Obtain a class enumerator for the video compressor category.
-    pSysDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnumCat, 0);
-    if (!pEnumCat) {
-        return E_FAIL;
-    }
+	// Obtain a class enumerator for the video compressor category.
+	pSysDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, & pEnumCat, 0);
+	if (!pEnumCat) {
+		return E_FAIL;
+	}
 
-    pEnumCat->Reset();
+	pEnumCat->Reset();
 
-    // Enumerate the monikers.
-    //CComPtr<IMoniker> pMoniker;
+	// Enumerate the monikers.
+	//CComPtr<IMoniker> pMoniker;
 
-    while(1) {
-    //while(pMoniker && pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK)
-        CComPtr<IMoniker> pMoniker;
-        ULONG cFetched;
-        CComPtr<IPropertyBag> pProp;
+	while (true) {
+		//while(pMoniker && pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK)
+		CComPtr < IMoniker > pMoniker;
+		ULONG cFetched;
+		CComPtr < IPropertyBag > pProp;
 
-        hr_work = pEnumCat->Next(1, &pMoniker, &cFetched);
-        if (hr_work != S_OK) {
-            break;
-        }
+		hr_work = pEnumCat->Next(1, & pMoniker, & cFetched);
+		if (hr_work != S_OK) {
+			break;
+		}
 
-        hr = pMoniker->BindToStorage(0, 0,
-                IID_IPropertyBag, (void **)&pProp);
+		hr = pMoniker->BindToStorage(0, 0,
+		IID_IPropertyBag, (void * *) & pProp);
 
-        if (hr != S_OK) {
-            continue;
-        }
+		if (hr != S_OK) {
+			continue;
+		}
 
-        VARIANT varName;
-        VariantInit(&varName); // Try to match the friendly name.
-        hr = pProp->Read(L"FriendlyName", &varName, 0);
-        if (SUCCEEDED(hr) && (wcscmp(bstrName, varName.bstrVal) == 0))
-        {
-            hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)&pFilter);
-            break;
-        }
-        VariantClear(&varName);
-        pMoniker = NULL; // Release for the next loop.
-    }
-    if (pFilter) {
-        *pF = pFilter;
-        (*pF)->AddRef();  // Add ref on the way out.
-    }
-    return hr;
+		VARIANT varName;
+		VariantInit(& varName); // Try to match the friendly name.
+		hr = pProp->Read(L"FriendlyName", & varName, 0);
+		if (SUCCEEDED(hr) && (wcscmp(bstrName, varName.bstrVal) == 0)) {
+			hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void * *) & pFilter);
+			break;
+		}
+		VariantClear(& varName);
+		pMoniker = NULL; // Release for the next loop.
+	}
+	if (pFilter) {
+		* pF = pFilter;
+		(* pF)->AddRef(); // Add ref on the way out.
+	}
+	return hr;
 }
 
+CComPtr < IBaseFilter > GetCaptureDevice(char * device_name) {
+	CComPtr < IBaseFilter > ppCap = NULL;
+	//CComPtr< IBaseFilter > pCap;
+	HRESULT hr;
 
+	// create an enumerator
+	CComPtr < ICreateDevEnum > pCreateDevEnum;
+	pCreateDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
+	if (!pCreateDevEnum) {
+		return NULL;
+	}
 
-CComPtr< IBaseFilter > GetCaptureDevice(char * device_name) {
-    CComPtr< IBaseFilter > ppCap = NULL;
-    //CComPtr< IBaseFilter > pCap;
-    HRESULT hr;
+	// enumerate video capture devices
+	CComPtr < IEnumMoniker > pEm;
+	pCreateDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, & pEm, 0);
+	if (!pEm) {
+		return NULL;
+	}
 
-    // create an enumerator
-    CComPtr< ICreateDevEnum > pCreateDevEnum;
-    pCreateDevEnum.CoCreateInstance( CLSID_SystemDeviceEnum );
-    if( !pCreateDevEnum ) {
-        return NULL;
-    }
+	pEm->Reset();
 
-    // enumerate video capture devices
-    CComPtr< IEnumMoniker > pEm;
-    pCreateDevEnum->CreateClassEnumerator( CLSID_VideoInputDeviceCategory, &pEm, 0 );
-    if( !pEm ) {
-        return NULL;
-    }
+	// go through and find first video capture device
+	while (true) {
+		ULONG ulFetched = 0;
+		CComPtr < IMoniker > pM;
 
-    pEm->Reset();
+		hr = pEm->Next(1, & pM, & ulFetched);
+		if (hr != S_OK)
+			break;
 
-    // go through and find first video capture device
-    while( 1 ) {
-        ULONG ulFetched = 0;
-        CComPtr< IMoniker > pM;
+		// get the property bag
+		CComPtr < IPropertyBag > pBag;
+		hr = pM->BindToStorage(0, 0, IID_IPropertyBag, (void * *) & pBag);
+		if (hr != S_OK) {
+			continue;
+		}
 
-        hr = pEm->Next( 1, &pM, &ulFetched );
-        if( hr != S_OK )
-            break;
+		// ask for the english-readable name
+		CComVariant var;
+		var.vt = VT_BSTR;
+		hr = pBag->Read(L"FriendlyName", & var, NULL);
+		if (hr != S_OK) {
+			continue;
+		}
 
-        // get the property bag
-        CComPtr< IPropertyBag > pBag;
-        hr = pM->BindToStorage( 0, 0, IID_IPropertyBag, (void**) &pBag );
-        if( hr != S_OK ) {
-            continue;
-        }
-
-        // ask for the english-readable name
-        CComVariant var;
-        var.vt = VT_BSTR;
-        hr = pBag->Read( L"FriendlyName", &var, NULL);
-        if( hr != S_OK ) {
-            continue;
-        }
-
-        //QString temp = (QString)_bstr_t(var);
-        char * temp = (char*)_bstr_t(var);
-        if( temp == device_name) {
-            hr = pM->BindToObject( 0, 0, IID_IBaseFilter, (void**) &ppCap );
-            if( ppCap ) {
-                break;
-            }
-        }
-    }
-    return ppCap;
+		//QString temp = (QString)_bstr_t(var);
+		char * temp = (char *) _bstr_t(var);
+		if (temp == device_name) {
+			hr = pM->BindToObject(0, 0, IID_IBaseFilter, (void * *) & ppCap);
+			if (ppCap) {
+				break;
+			}
+		}
+	}
+	return ppCap;
 }
 
 /*
-*   take a base filter and try to return a pointer to its IAMStreamConfig
-*   return a NULL pointer in the case it fails
-*/
-IAMStreamConfig * GetIAMStreamConfig(IBaseFilter *pFilter) {
-    IAMStreamConfig * pIAMS = NULL;
-    IEnumPins *pEnum = 0;
-    IPin *pPin = 0;
-    HRESULT hr = pFilter->EnumPins(&pEnum);
-    if (FAILED(hr)) {
-        return NULL;
-    }
+ * Takes a base filter and try to return a pointer to its IAMStreamConfig
+ * return a NULL pointer in the case it fails
+ */
+IAMStreamConfig * GetIAMStreamConfig(IBaseFilter * pFilter) {
+	IAMStreamConfig * pIAMS = NULL;
+	IEnumPins * pEnum = 0;
+	IPin * pPin = 0;
+	HRESULT hr = pFilter->EnumPins(& pEnum);
+	if (FAILED(hr)) {
+		return NULL;
+	}
 
-    while (pEnum->Next(1, &pPin, NULL) == S_OK) {
-        hr = pPin->QueryInterface(IID_IAMStreamConfig, (void**)&pIAMS);
-        if(hr == S_OK) {
-            return  pIAMS;
-        }
-        pPin->Release();
-    }
-    pEnum->Release();
-    return NULL;
+	while (pEnum->Next(1, & pPin, NULL) == S_OK) {
+		hr = pPin->QueryInterface(IID_IAMStreamConfig, (void * *) & pIAMS);
+		if (hr == S_OK) {
+			return pIAMS;
+		}
+		pPin->Release();
+	}
+	pEnum->Release();
+	return NULL;
 }
 
-HRESULT ConnectFilters(IGraphBuilder *pGraph, IPin *pOut, IBaseFilter *pDest) {
-    if ((pGraph == NULL) || (pOut == NULL) || (pDest == NULL)) {
-        return E_POINTER;
-    }
-    #ifdef debug
-        PIN_DIRECTION PinDir;
-        pOut->QueryDirection(&PinDir);
-        _ASSERTE(PinDir == PINDIR_OUTPUT);
-    #endif
+HRESULT ConnectFilters(IGraphBuilder * pGraph, IPin * pOut, IBaseFilter * pDest) {
+	if ((pGraph == NULL) || (pOut == NULL) || (pDest == NULL)) {
+		return E_POINTER;
+	}
+#ifdef debug
+	PIN_DIRECTION PinDir;
+	pOut->QueryDirection(& PinDir);
+	_ASSERTE(PinDir == PINDIR_OUTPUT);
+#endif
 
-    // Find an input pin on the downstream filter.
-    IPin *pIn = 0;
-    HRESULT hr = GetUnconnectedPin(pDest, PINDIR_INPUT, &pIn);
-    if (FAILED(hr)) {
-        return hr;
-    }
-    // Try to connect them.
-    hr = pGraph->Connect(pOut, pIn);
-    pIn->Release();
-    return hr;
+	// Find an input pin on the downstream filter.
+	IPin * pIn = 0;
+	HRESULT hr = GetUnconnectedPin(pDest, PINDIR_INPUT, & pIn);
+	if (FAILED(hr)) {
+		return hr;
+	}
+	// Try to connect them.
+	hr = pGraph->Connect(pOut, pIn);
+	pIn->Release();
+	return hr;
 }
 
-HRESULT ConnectFilters(IGraphBuilder *pGraph, IBaseFilter *pSrc, IBaseFilter *pDest) {
-    if ((pGraph == NULL) || (pSrc == NULL) || (pDest == NULL)) {
-        return E_POINTER;
-    }
+HRESULT ConnectFilters(IGraphBuilder * pGraph, IBaseFilter * pSrc, IBaseFilter * pDest) {
+	if ((pGraph == NULL) || (pSrc == NULL) || (pDest == NULL)) {
+		return E_POINTER;
+	}
 
-    // Find an output pin on the first filter.
-    IPin *pOut = 0;
-    HRESULT hr = GetUnconnectedPin(pSrc, PINDIR_OUTPUT, &pOut);
-    if (FAILED(hr)) {
-        return hr;
-    }
+	// Find an output pin on the first filter.
+	IPin * pOut = 0;
+	HRESULT hr = GetUnconnectedPin(pSrc, PINDIR_OUTPUT, & pOut);
+	if (FAILED(hr)) {
+		return hr;
+	}
 
-    hr = ConnectFilters(pGraph, pOut, pDest);
-    pOut->Release();
-    return hr;
+	hr = ConnectFilters(pGraph, pOut, pDest);
+	pOut->Release();
+	return hr;
 }
 
-/*  GetUnconnectedPin
-*
-*   get filter's unconnected pin if any
-*/
-HRESULT GetUnconnectedPin(IBaseFilter *pFilter, PIN_DIRECTION PinDir, IPin **ppPin) {
-    *ppPin = 0;
-    IEnumPins *pEnum = 0;
-    IPin *pPin = 0;
-    HRESULT hr = pFilter->EnumPins(&pEnum);
-    if (FAILED(hr)) {
-        return hr;
-    }
-    while (pEnum->Next(1, &pPin, NULL) == S_OK) {
-        PIN_DIRECTION ThisPinDir;
-        pPin->QueryDirection(&ThisPinDir);
-        if (ThisPinDir == PinDir) {
-            IPin *pTmp = 0;
-            hr = pPin->ConnectedTo(&pTmp);
-            // Already connected, not the pin we want.
-            if (SUCCEEDED(hr)) {
-                pTmp->Release();
-            }
-            // Unconnected, this is the pin we want.
-            else {
-                pEnum->Release();
-                *ppPin = pPin;
-                return S_OK;
-            }
-        }
-        pPin->Release();
-    }
-    pEnum->Release();
-    // Did not find a matching pin.
-    return E_FAIL;
+/*
+ * GetUnconnectedPin
+ *
+ * Get filter's unconnected pin if any
+ */
+HRESULT GetUnconnectedPin(IBaseFilter * pFilter, PIN_DIRECTION PinDir, IPin * * ppPin) {
+	* ppPin = 0;
+	IEnumPins * pEnum = 0;
+	IPin * pPin = 0;
+	HRESULT hr = pFilter->EnumPins(& pEnum);
+	if (FAILED(hr)) {
+		return hr;
+	}
+	while (pEnum->Next(1, & pPin, NULL) == S_OK) {
+		PIN_DIRECTION ThisPinDir;
+		pPin->QueryDirection(& ThisPinDir);
+		if (ThisPinDir == PinDir) {
+			IPin * pTmp = 0;
+			hr = pPin->ConnectedTo(& pTmp);
+			// Already connected, not the pin we want.
+			if (SUCCEEDED(hr)) {
+				pTmp->Release();
+			}
+			// Unconnected, this is the pin we want.
+			else {
+				pEnum->Release();
+				* ppPin = pPin;
+				return S_OK;
+			}
+		}
+		pPin->Release();
+	}
+	pEnum->Release();
+	// Did not find a matching pin.
+	return E_FAIL;
 }
 
-void GetDefaultCapDevice(IBaseFilter ** ppCap) {
-    HRESULT hr;
+void GetDefaultCapDevice(IBaseFilter * * ppCap) {
+	HRESULT hr;
 
-    _ASSERTE(ppCap);
-    if (!ppCap) {
-        return;
-    }
+	_ASSERTE(ppCap);
+	if (!ppCap) {
+		return;
+	}
 
-    *ppCap = NULL;
+	* ppCap = NULL;
 
-    // create an enumerator
-    CComPtr< ICreateDevEnum > pCreateDevEnum;
-    pCreateDevEnum.CoCreateInstance( CLSID_SystemDeviceEnum );
+	// create an enumerator
+	CComPtr < ICreateDevEnum > pCreateDevEnum;
+	pCreateDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
 
-    _ASSERTE(pCreateDevEnum);
-    if( !pCreateDevEnum ) {
-        return;
-    }
+	_ASSERTE(pCreateDevEnum);
+	if (!pCreateDevEnum) {
+		return;
+	}
 
-    // enumerate video capture devices
-    CComPtr< IEnumMoniker > pEm;
-    pCreateDevEnum->CreateClassEnumerator( CLSID_VideoInputDeviceCategory, &pEm, 0 );
+	// enumerate video capture devices
+	CComPtr < IEnumMoniker > pEm;
+	pCreateDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, & pEm, 0);
 
-    //_ASSERTE(pEm);
-    if( !pEm ) {
-        return;
-    }
+	//_ASSERTE(pEm);
+	if (!pEm) {
+		return;
+	}
 
-    pEm->Reset( );
+	pEm->Reset();
 
-    // go through and find first video capture device
-    while(1) {
-        ULONG ulFetched = 0;
-        CComPtr< IMoniker > pM;
+	// go through and find first video capture device
+	while (true) {
+		ULONG ulFetched = 0;
+		CComPtr < IMoniker > pM;
 
-        hr = pEm->Next( 1, &pM, &ulFetched );
-        if( hr != S_OK ) {
-            break;
-        }
+		hr = pEm->Next(1, & pM, & ulFetched);
+		if (hr != S_OK) {
+			break;
+		}
 
-        // get the property bag
-        CComPtr< IPropertyBag > pBag;
-        hr = pM->BindToStorage( 0, 0, IID_IPropertyBag, (void**) &pBag );
-        if( hr != S_OK ) {
-            continue;
-        }
+		// get the property bag
+		CComPtr < IPropertyBag > pBag;
+		hr = pM->BindToStorage(0, 0, IID_IPropertyBag, (void * *) & pBag);
+		if (hr != S_OK) {
+			continue;
+		}
 
-        // ask for the english-readable name
-        CComVariant var;
-        var.vt = VT_BSTR;
-        hr = pBag->Read( L"FriendlyName", &var, NULL);
-        if( hr != S_OK ) {
-            continue;
-        }
+		// ask for the english-readable name
+		CComVariant var;
+		var.vt = VT_BSTR;
+		hr = pBag->Read(L"FriendlyName", & var, NULL);
+		if (hr != S_OK) {
+			continue;
+		}
 
-        // ask for the actual filter
-        hr = pM->BindToObject( 0, 0, IID_IBaseFilter, (void**) ppCap );
-        if( *ppCap ) {
-            break;
-        }
-    }
+		// ask for the actual filter
+		hr = pM->BindToObject(0, 0, IID_IBaseFilter, (void * *) ppCap);
+		if (* ppCap) {
+			break;
+		}
+	}
 }
-
