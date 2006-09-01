842c891852d6da420343e051296060dcbd23b6e8
Andreas Schneider <mail@cynapses.org>
2006-09-01 11:11:39 +0000

    Add linux build script for cmake

---
+++ build/build_make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export TERM=xterm
+
+SCRIPT="$0"
+COUNT=0
+while [ -L "${SCRIPT}" ]
+do
+	SCRIPT=$(readlink ${SCRIPT})
+	COUNT=$(expr ${COUNT} + 1)
+	if [ ${COUNT} -gt 100 ]; then
+		echo "Too many symbolic links"
+		exit 1
+	fi
+done
+BUILDDIR=$(dirname ${SCRIPT})
+
+cd ${BUILDDIR}
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr "$@" && make
+
