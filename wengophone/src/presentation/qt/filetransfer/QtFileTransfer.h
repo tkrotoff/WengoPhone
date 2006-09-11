626f49479efcb6a56a212711c4ee952e5025b1dd
Mathieu Stute <mathieu.stute@wengo.com>
2006-09-11 10:46:48 +0000

    fix

--- wengophone/src/presentation/qt/filetransfer/QtFileTransfer.h
+++ wengophone/src/presentation/qt/filetransfer/QtFileTransfer.h
@@ -56,6 +56,10 @@ public:
 
 	void addSendFileSession(SendFileSession * fileSession);
 
+	QtFileTransferWidget * getFileTransferWidget() {
+		return _qtFileTransferWidget;
+	}
+
 Q_SIGNALS:
 
 	void newReceiveFileSessionCreatedEventHandlerSignal(ReceiveFileSession * fileSession);
