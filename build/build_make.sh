b9c401aa40ecd2b817b4dd76ca5c2ee570cd331c
Andreas Schneider <mail@cynapses.org>
2006-09-18 12:14:49 +0000

    Added owphone Added qtwengophone (yeeehaaa!)

--- build/build_make.sh
+++ build/build_make.sh
@@ -19,23 +19,24 @@ cd ${BUILDDIR}
 
 case $1 in
 	configure)
-		cmake -DCMAKE_INSTALL_PREFIX=/usr "$@" ..
+		DOT=$(which dot)
+		if [ -n "${DOT}" ]; then
+			cmake -DCMAKE_INSTALL_PREFIX=/usr --graphviz=${BUILDDIR}/wengophone.dot "$@" ..
+			${DOT} -Tpng -o${BUILDDIR}/wengophone.png ${BUILDDIR}/wengophone.dot
+			${DOT} -Tsvg -o${BUILDDIR}/wengophone.svg ${BUILDDIR}/wengophone.dot
+		else
+			cmake -DCMAKE_INSTALL_PREFIX=/usr "$@" ..
+		fi
 	;;
 	make)
 		cmake -DCMAKE_INSTALL_PREFIX=/usr "$@" ..
 		make
 	;;
-	graph)
-		cmake -DCMAKE_INSTALL_PREFIX=/usr --graphviz=${BUILDDIR}/wengophone.dot "$@" ..
-		dot -Tpng -o${BUILDDIR}/wengophone.png ${BUILDDIR}/wengophone.dot
-		dot -Tsvg -o${BUILDDIR}/wengophone.svg ${BUILDDIR}/wengophone.dot
-	;;
 	*)
 		echo "Usage: $(basename $0) (configure|make|graph)"
 		echo
-		echo "    configure - run cmake configure"
-		echo "    make - run configure and build wengophone"
-		echo "    graph - build a picture of the deps"
+		echo "  configure - run cmake configure"
+		echo "  make - run configure and build wengophone"
 		echo
 	;;
 esac
--- cmake/Modules/DefineWengoOptions.cmake
+++ cmake/Modules/DefineWengoOptions.cmake
@@ -2,3 +2,5 @@ if (WIN32)
   option(WITH_PORTAUDIO_DSOUND "Should Portaudio compiled with DirectSound support" OFF)
 endif (WIN32)
 
+option(WITH_VIDEO "Build phapi with video support" ON)
+option(WITH_BUILDID "Build wengophone with buildid (builds faster without)" ON)
