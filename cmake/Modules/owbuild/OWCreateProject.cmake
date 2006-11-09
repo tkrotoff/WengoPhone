132d8a734fae10ed584c243ccaf319093e94a0fc
Tanguy Krotoff <tkrotoff@gmail.com>
2006-11-09 18:05:09 +0000

    * CMake support improved

--- cmake/Modules/owbuild/OWCreateProject.cmake
+++ cmake/Modules/owbuild/OWCreateProject.cmake
@@ -22,14 +22,24 @@ macro (OW_CREATE_PROJECT name)
 	set(${PROJECT_NAME}_LIBRARIES "" CACHE INTERNAL "${PROJECT_NAME} public libraries")
 	set(${PROJECT_NAME}_PRIVATE_LIBRARIES "")
 
+	# Resets link directories (e.g library directories, e.g library paths)
+	set_directory_properties(PROPERTIES LINK_DIRECTORIES "")
 	set(${PROJECT_NAME}_LIBRARY_DIRS "" CACHE INTERNAL "${PROJECT_NAME} public library directories")
 	set(${PROJECT_NAME}_PRIVATE_LIBRARY_DIRS "" CACHE INTERNAL "${PROJECT_NAME} private library directories")
 
+	# Resets the definitions
+	set_directory_properties(PROPERTIES DEFINITIONS "")
+	set_directory_properties(PROPERTIES DEFINITION "")
 	set(${PROJECT_NAME}_DEFINITIONS "" CACHE INTERNAL "${PROJECT_NAME} public definitions")
 	set(${PROJECT_NAME}_PRIVATE_DEFINITIONS "")
 
+	# Resets compile flags
 	set(${PROJECT_NAME}_PRIVATE_COMPILE_FLAGS "")
+
+	# Resets link flags
 	set(${PROJECT_NAME}_PRIVATE_LINK_FLAGS "")
+
+	# Resets build version and API version
 	set(${PROJECT_NAME}_BUILD_VERSION "")
 	set(${PROJECT_NAME}_API_VERSION "")
 
