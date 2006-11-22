fc5793178f8efde4352389861996e5d38eca2fde
Tanguy Krotoff <tkrotoff@gmail.com>
2006-11-22 13:18:21 +0000

    * (compilation fix) FFmpeg rev7070 fixed for Visual C++ 7.1

--- libs/3rdparty/ffmpeg/include/ffmpeg/common.h
+++ libs/3rdparty/ffmpeg/include/ffmpeg/common.h
@@ -26,6 +26,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#ifdef _MSC_VER
+#define inline __inline
+#endif
+
 #ifndef M_PI
 #define M_PI    3.14159265358979323846
 #endif
@@ -84,7 +88,24 @@
 #endif
 #endif
 
+#ifndef __GNU_C__
+#    define EMULATE_INTTYPES
+#endif
+
+
+#ifndef EMULATE_INTTYPES
 #   include <inttypes.h>
+#else
+    typedef signed char  int8_t;
+    typedef signed short int16_t;
+    typedef signed int   int32_t;
+    typedef unsigned char  uint8_t;
+    typedef unsigned short uint16_t;
+    typedef unsigned int   uint32_t;
+    typedef signed long long   int64_t;
+    typedef unsigned long long uint64_t;
+#endif /* EMULATE_INTTYPES */
+
 
 #ifndef PRId64
 #define PRId64 "lld"
