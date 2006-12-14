6c2eaa2d6b39592bd34e343221f4428c9b62168e
Tanguy Krotoff <tkrotoff@gmail.com>
2006-12-14 18:21:39 +0000

    * (bugfix) new compiler flags were introducing bugs

--- DefineCompilerFlags.cmake
+++ DefineCompilerFlags.cmake
@@ -32,9 +32,9 @@ endif (CMAKE_BUILD_TYPE MATCHES Debug)
 
 # Enable warnings
 if (MSVC)
-	set(CMAKE_C_FLAGS "/W4")
-	set(CMAKE_CXX_FLAGS "/W4")
+	#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4")
+	#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
 else (MSVC)
-	set(CMAKE_C_FLAGS "-Wall")
-	set(CMAKE_CXX_FLAGS "-Wall")
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 endif (MSVC)
