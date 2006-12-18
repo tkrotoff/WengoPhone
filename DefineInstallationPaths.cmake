5405d8e7fd224d576291d010f6d2c0ab0c4479a4
Tanguy Krotoff <tkrotoff@gmail.com>
2006-12-18 15:01:23 +0000

    * CMakeLists-install-linux.txt updated

--- DefineInstallationPaths.cmake
+++ DefineInstallationPaths.cmake
@@ -1,4 +1,16 @@
-set(FRAMEWORK_INSTALL_DIR ${APPLICATION_NAME}.app/Contents/Frameworks)
+if (APPLE)
+	set(FRAMEWORK_INSTALL_DIR ${APPLICATION_NAME}.app/Contents/Frameworks
+		CACHE PATH "Framework installation path (MacOSX only)")
 
-set(DATA_INSTALL_DIR ${APPLICATION_NAME}.app/Contents/Resources
-	CACHE PATH "Data/resources installation path")
+	set(DATA_INSTALL_DIR ${APPLICATION_NAME}.app/Contents/Resources
+		CACHE PATH "Data/resources (sounds, translations...) installation path")
+endif (APPLE)
+
+if (UNIX AND NOT APPLE)
+	set(DATA_INSTALL_DIR "/usr/share/wengophone"
+		CACHE PATH "Data/resources (sounds, translations...) installation path")
+	set(APPLICATION_BIN_INSTALL_DIR "/usr/bin"
+		CACHE PATH "Application binary installation path")
+	set(LIBRARIES_INSTALL_DIR "/usr/lib/wengophone"
+		CACHE PATH "Libraries installation path")
+endif (UNIX AND NOT APPLE)
