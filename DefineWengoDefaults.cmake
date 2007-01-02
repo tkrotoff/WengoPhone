a5988187e30e1f79825985e5291f5371c5bfea70
Tanguy Krotoff <tkrotoff@gmail.com>
2007-01-02 17:38:32 +0000

    * typo correction

--- DefineWengoDefaults.cmake
+++ DefineWengoDefaults.cmake
@@ -1,20 +1,20 @@
-# Required cmake version
+# Required CMake version
 cmake_minimum_required(VERSION 2.4.3)
 
 # Always include srcdir and builddir in include path
 # This saves typing ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} in
 # about every subdir
-# since cmake 2.4.0
+# since CMake 2.4.0
 set(CMAKE_INCLUDE_CURRENT_DIR OFF)
 
 # Put the include dirs which are in the source or build tree
 # before all other include dirs, so the headers in the sources
 # are prefered over the already installed ones
-# since cmake 2.4.1
+# since CMake 2.4.1
 set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON)
 
 # Use colored output
-# since cmake 2.4.0
+# since CMake 2.4.0
 set(CMAKE_COLOR_MAKEFILE ON)
 
 # Define the generic version of the libraries here
